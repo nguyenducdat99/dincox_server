439742019e7ba1fd4b8e5c67cd7e7fb8f0f50d45
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-03-09 14:28:14 +0700

    add access control allow origin, method, header 2:27pm 09/03/2021

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const POST = process.env.POST || 8080; // declare port
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// allow access origin
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
+
 // use router
 let routers = require('./api/routers/routers');
 routers(app);
