07bd8f6d07235ee44b401c1777251d4db10dd49a
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-03-10 09:07:22 +0700

    install cors and use core for server 9:06am 10/03/2021

--- server.js
+++ server.js
@@ -3,18 +3,14 @@ let app = express();
 const bodyParser = require('body-parser'); // require body parser
 require('dotenv').config(); //config dotent
 const POST = process.env.POST || 8080; // declare port
+const cors = require('cors');
 
 // use bodyParser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // allow access origin
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
+app.use(cors());
 
 // use router
 let routers = require('./api/routers/routers');
