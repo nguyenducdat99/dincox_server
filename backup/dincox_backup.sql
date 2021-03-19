37a190c263850d003881066820ffe3e9a78957f8
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-03-19 18:23:27 +0700

    update dincox backup file 6:23pm 19/03/2021

--- backup/dincox_backup.sql
+++ backup/dincox_backup.sql
@@ -38,7 +38,7 @@ CREATE TABLE `tblaccounts` (
   UNIQUE KEY `username_UNIQUE` (`user_name`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `mobile_UNIQUE` (`mobile`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='store account user';
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='store account user';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +47,7 @@ CREATE TABLE `tblaccounts` (
 
 LOCK TABLES `tblaccounts` WRITE;
 /*!40000 ALTER TABLE `tblaccounts` DISABLE KEYS */;
-INSERT INTO `tblaccounts` VALUES (20,'datnd','1',0,'datnd@gmail.com',NULL,NULL,NULL,NULL,1),(23,'manager','1',1,'manager@gmail.com',NULL,NULL,NULL,NULL,0),(24,'thuanpt','1',0,'thuanpt@gmail.com',NULL,NULL,NULL,NULL,0),(25,'thunt','1',0,'thunt@gmail.com',NULL,NULL,NULL,NULL,0),(26,'lyht','1234',0,'lyht@gmail.com',NULL,NULL,NULL,NULL,0),(27,'vinhhq','1',0,'vinhhq@gmail.com',NULL,NULL,NULL,NULL,0),(28,'thaont','1',0,'thaont@gmail.com',NULL,NULL,NULL,NULL,0),(29,'hiennq','1',0,'hiennq@gmail.com',NULL,NULL,NULL,NULL,0),(30,'duongvd','1',0,'duongvd@gmail.com',NULL,NULL,NULL,NULL,0),(31,'baodt','1',0,'baodt@gmail.com',NULL,NULL,NULL,NULL,0),(32,'dongnc','1',0,'dongnc@gmail.com',NULL,NULL,NULL,NULL,0),(33,'khiemnd','1',0,'khiemnd@gmail.com',NULL,NULL,NULL,NULL,0),(34,'super','1',2,'super@gmail.com',NULL,NULL,NULL,NULL,1);
+INSERT INTO `tblaccounts` VALUES (20,'datnd','1',0,'datnd@gmail.com',NULL,NULL,NULL,NULL,1),(23,'manager','1',1,'manager@gmail.com',NULL,NULL,NULL,NULL,0),(24,'thuanpt','1',0,'thuanpt@gmail.com',NULL,NULL,NULL,NULL,0),(25,'thunt','1',0,'thunt@gmail.com',NULL,NULL,NULL,NULL,0),(26,'lyht','1234',0,'lyht@gmail.com',NULL,NULL,NULL,NULL,0),(27,'vinhhq','1',0,'vinhhq@gmail.com',NULL,NULL,NULL,NULL,0),(28,'thaont','1',0,'thaont@gmail.com',NULL,NULL,NULL,NULL,0),(29,'hiennq','1',0,'hiennq@gmail.com',NULL,NULL,NULL,NULL,0),(30,'duongvd','1',0,'duongvd@gmail.com',NULL,NULL,NULL,NULL,0),(31,'baodt','1',0,'baodt@gmail.com',NULL,NULL,NULL,NULL,0),(32,'dongnc','1',0,'dongnc@gmail.com',NULL,NULL,NULL,NULL,0),(33,'khiemnd','1',0,'khiemnd@gmail.com',NULL,NULL,NULL,NULL,0),(34,'super','1',2,'super@gmail.com',NULL,NULL,NULL,NULL,1),(36,'duydv','1',0,'duydv@gmail.com',NULL,NULL,NULL,NULL,0),(37,'static','1',0,'static@gmail.com',NULL,NULL,NULL,NULL,1),(38,'dunglv','1',0,'dunglv@gmail.com',NULL,NULL,NULL,NULL,0);
 /*!40000 ALTER TABLE `tblaccounts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -61,8 +61,8 @@ DROP TABLE IF EXISTS `tblcategories`;
 CREATE TABLE `tblcategories` (
   `id_category` int unsigned NOT NULL AUTO_INCREMENT,
   `category_name` varchar(45) NOT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `edited_at` int DEFAULT NULL,
+  `created_at` varchar(10) DEFAULT NULL,
+  `edited_at` varchar(10) DEFAULT NULL,
   `status` int DEFAULT NULL,
   PRIMARY KEY (`id_category`),
   UNIQUE KEY `id_category_UNIQUE` (`id_category`)
@@ -122,18 +122,18 @@ CREATE TABLE `tblimages` (
   `id_image` int unsigned NOT NULL AUTO_INCREMENT,
   `id_product` int unsigned DEFAULT NULL,
   `id_new` int unsigned DEFAULT NULL,
-  `title` varchar(45) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `edited_at` datetime DEFAULT NULL,
-  `path` varchar(45) NOT NULL,
-  `status` bit(1) DEFAULT NULL,
+  `path` varchar(100) NOT NULL,
+  `status` int DEFAULT NULL,
   PRIMARY KEY (`id_image`),
   UNIQUE KEY `id_image_UNIQUE` (`id_image`),
   KEY `fk_images_products_idx` (`id_product`),
   KEY `fk_images_news_idx` (`id_new`),
   CONSTRAINT `fk_images_news` FOREIGN KEY (`id_new`) REFERENCES `tblnews` (`id_new`),
   CONSTRAINT `fk_images_products` FOREIGN KEY (`id_product`) REFERENCES `tblproducts` (`id_product`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -142,7 +142,7 @@ CREATE TABLE `tblimages` (
 
 LOCK TABLES `tblimages` WRITE;
 /*!40000 ALTER TABLE `tblimages` DISABLE KEYS */;
-INSERT INTO `tblimages` VALUES (1,1,NULL,'Flower',NULL,NULL,'./Flower.jpg',_binary ''),(2,NULL,NULL,'Car',NULL,NULL,'./images/car.jpg',_binary ''),(3,NULL,NULL,'house',NULL,NULL,'./images/house.jpg',_binary '');
+INSERT INTO `tblimages` VALUES (6,1,NULL,'D21HI CHOC',NULL,NULL,'/images/D21HI_CHOC.jpg',1),(7,2,NULL,'D27 DENIM',NULL,NULL,'/images/D27_DENIM.jpg',1),(8,3,NULL,'COX007GREY',NULL,NULL,'/images/COX007GREY.jpg',1),(9,4,NULL,'C38 WHITE',NULL,NULL,'/images/C38_WHITE.jpg',1),(10,5,NULL,'C38 FULLBLACK',NULL,NULL,'/images/C38_FULLBLACK.jpg',1),(11,6,NULL,'D21HI BLACK',NULL,NULL,'/images/D21HI_BLACK.jpg',1),(12,7,NULL,'D21HI WHITE',NULL,NULL,'/images/D21HI_WHITE.jpg',1),(13,8,NULL,'D20 WHITE',NULL,NULL,'/images/D20_WHITE.jpg',1),(14,9,NULL,'D28 BLACK',NULL,NULL,'/images/D28_BLACK.jpg',1),(15,10,NULL,'COX007TAN',NULL,NULL,'/images/COX007TAN.jpg',1),(16,11,NULL,'D23 WHITE',NULL,NULL,'/images/D23_WHITE.jpg',1),(17,12,NULL,'D21 CHOCOLATE',NULL,NULL,'/images/D21_CHOCOLATE.jpg',1),(18,13,NULL,'D28 BLACK',NULL,NULL,'/images/D28_BLACK.jpg',1),(19,14,NULL,'C01 OFFW/RED',NULL,NULL,'/images/C01OFFW_RED.jpg',1),(20,15,NULL,'C01 OFFW/YELLOW',NULL,NULL,'/images/C01OFFW_YELLOW.jpg',1),(21,1,NULL,'D21HI CHOC',NULL,NULL,'/images/D21_CHOCOLATE.jpg',1),(22,2,NULL,'D27 DENIM',NULL,NULL,'/images/D21HI_WHITE.jpg',1),(23,16,NULL,'SD05 RED',NULL,NULL,'/images/SD05_RED.jpg',1),(24,1,NULL,'D21HI CHOC',NULL,NULL,'/images/D21HI_CHOC_V2.jpg',1),(25,NULL,1,'CÁC MẪU GIÀY PHÙ HỢP MANG ĐI HỌC GIÁ SINH VIÊN , HỌC SINH',NULL,NULL,'/images/article1.jpg',1),(26,NULL,2,'dincox news',NULL,NULL,'/images/article2.jpg',1),(27,NULL,3,'dincox news',NULL,NULL,'/images/article3.jpg',1),(28,NULL,4,'dincox news',NULL,NULL,'/images/article4.jpg',1),(29,NULL,5,'dincox news',NULL,NULL,'/images/article8.jpg',1),(30,NULL,6,'dincox news',NULL,NULL,'/images/article5.jpg',1),(31,NULL,7,'dincox news',NULL,NULL,'/images/article6.jpg',1),(32,NULL,8,'dincox news',NULL,NULL,'/images/article7.jpg',1),(33,NULL,8,'dincox news',NULL,NULL,'/images/article7.jpg',1);
 /*!40000 ALTER TABLE `tblimages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -187,7 +187,8 @@ DROP TABLE IF EXISTS `tblorderdetails`;
 CREATE TABLE `tblorderdetails` (
   `id_order` int unsigned NOT NULL,
   `id_product` int unsigned NOT NULL,
-  `size` varchar(45) NOT NULL,
+  `discount` int DEFAULT NULL,
+  `size` int NOT NULL,
   `quantity` int NOT NULL,
   PRIMARY KEY (`id_order`,`id_product`),
   KEY `fk_orderdetailt_products_idx` (`id_product`),
@@ -202,6 +203,7 @@ CREATE TABLE `tblorderdetails` (
 
 LOCK TABLES `tblorderdetails` WRITE;
 /*!40000 ALTER TABLE `tblorderdetails` DISABLE KEYS */;
+INSERT INTO `tblorderdetails` VALUES (8,1,0,1,1),(8,2,0,2,1),(8,3,25,1,1),(9,2,0,2,1),(9,3,25,1,1),(10,2,0,2,1),(10,3,25,1,1),(11,2,0,2,1),(11,3,25,1,1),(12,2,0,2,1),(12,3,25,1,1),(13,2,0,2,1),(13,3,25,1,1),(14,2,0,2,1),(14,3,25,1,1),(15,2,0,2,1),(15,3,25,1,1),(16,2,0,2,1),(16,3,25,1,1),(17,2,0,2,1),(17,3,25,1,1),(18,2,0,2,1),(18,3,25,1,1),(19,2,0,2,1),(19,3,25,1,1),(20,2,0,2,1),(20,3,25,1,1),(21,2,0,2,1),(21,3,25,1,1),(22,2,0,2,1),(22,3,25,1,1),(23,2,0,2,1),(23,3,25,1,1),(24,1,0,2,2),(25,1,0,3,1),(26,1,0,1,1),(27,1,0,1,1);
 /*!40000 ALTER TABLE `tblorderdetails` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -215,14 +217,17 @@ DROP TABLE IF EXISTS `tblorders`;
 CREATE TABLE `tblorders` (
   `id_order` int unsigned NOT NULL AUTO_INCREMENT,
   `id_account` int unsigned NOT NULL,
-  `create_at` datetime DEFAULT NULL,
+  `create_at` varchar(10) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
   `number_phone` varchar(45) DEFAULT NULL,
-  `reveiver` varchar(45) DEFAULT NULL,
+  `receiver` varchar(45) DEFAULT NULL,
   `sent_to` varchar(45) NOT NULL,
+  `transport_fee` int DEFAULT NULL,
+  `status` int DEFAULT NULL,
   UNIQUE KEY `id_order_UNIQUE` (`id_order`),
   KEY `fk_orders_account_idx` (`id_account`),
   CONSTRAINT `fk_orders_account` FOREIGN KEY (`id_account`) REFERENCES `tblaccounts` (`id_account`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -231,7 +236,7 @@ CREATE TABLE `tblorders` (
 
 LOCK TABLES `tblorders` WRITE;
 /*!40000 ALTER TABLE `tblorders` DISABLE KEYS */;
-INSERT INTO `tblorders` VALUES (1,26,NULL,'012397641','hainoi','hanoi');
+INSERT INTO `tblorders` VALUES (1,37,NULL,NULL,'012397641','hainoi','hanoi',NULL,NULL),(2,37,'18/03/2021','null','0123972342','admin1','hanoi,',NULL,1),(3,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',NULL,1),(4,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',NULL,1),(5,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',NULL,1),(6,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(7,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(8,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(9,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(10,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(11,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(12,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(13,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(14,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(15,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(16,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(17,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(18,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(19,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(20,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(21,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(22,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(23,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(24,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(25,37,'19/3/2021','dat@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(26,34,'19/3/2021','datnd@gmail.com','01234567890','Nguyễn Đức Đạt','Hà nội',33000,1),(27,37,'19/3/2021','dat@gmail.com','01234567789','duc dat','Hà Nội',33000,1);
 /*!40000 ALTER TABLE `tblorders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -258,7 +263,7 @@ CREATE TABLE `tblproducts` (
   UNIQUE KEY `id_product_UNIQUE` (`id_product`),
   KEY `fk_product_categories_idx` (`id_category`),
   CONSTRAINT `fk_product_categories` FOREIGN KEY (`id_category`) REFERENCES `tblcategories` (`id_category`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -267,7 +272,7 @@ CREATE TABLE `tblproducts` (
 
 LOCK TABLES `tblproducts` WRITE;
 /*!40000 ALTER TABLE `tblproducts` DISABLE KEYS */;
-INSERT INTO `tblproducts` VALUES (1,15,'D21HI CHOC',NULL,NULL,0,NULL,NULL,NULL,440000,1),(2,15,'D27 DENIM',NULL,NULL,0,NULL,NULL,NULL,430000,1),(3,13,'COX007GREY',NULL,NULL,1,NULL,NULL,NULL,250000,1),(4,12,'C38 WHITE',NULL,NULL,1,NULL,NULL,NULL,410000,1),(5,12,'C38 FULLBLACK',NULL,NULL,1,NULL,NULL,NULL,410000,1),(6,12,'D21HI BLACK',NULL,NULL,0,NULL,NULL,NULL,440000,1),(7,13,'D21HI WHITE',NULL,NULL,1,NULL,NULL,NULL,440000,1),(8,13,'D20 WHITE',NULL,NULL,0,NULL,NULL,NULL,490000,1),(9,14,'D28 BLACK',NULL,NULL,0,'',0,0,490000,1),(10,14,'COX007TAN',NULL,NULL,1,'',0,0,350000,1),(11,15,'D23 WHITE',NULL,NULL,1,'',1,1,375000,1);
+INSERT INTO `tblproducts` VALUES (1,15,'D21HI CHOC',NULL,NULL,0,NULL,NULL,NULL,440000,1),(2,15,'D27 DENIM',NULL,NULL,0,NULL,NULL,NULL,430000,1),(3,13,'COX007GREY',NULL,NULL,1,NULL,NULL,NULL,250000,1),(4,12,'C38 WHITE',NULL,NULL,1,NULL,NULL,NULL,410000,1),(5,12,'C38 FULLBLACK',NULL,NULL,0,NULL,NULL,NULL,410000,1),(6,12,'D21HI BLACK',NULL,NULL,0,NULL,NULL,NULL,440000,1),(7,13,'D21HI WHITE',NULL,NULL,1,NULL,NULL,NULL,440000,1),(8,13,'D20 WHITE',NULL,NULL,1,NULL,NULL,NULL,490000,1),(9,14,'D28 BLACK',NULL,NULL,0,'',0,0,490000,1),(10,14,'COX007TAN',NULL,NULL,0,'',0,0,350000,1),(11,15,'D23 WHITE',NULL,NULL,1,'',1,1,375000,1),(12,13,'D21 CHOCOLATE',NULL,NULL,1,'',0,0,430000,1),(13,15,'D28 BLACK',NULL,NULL,0,'',0,0,490000,1),(14,13,'C01 OFFW/RED',NULL,NULL,0,NULL,NULL,NULL,375000,1),(15,13,'C01 OFFW/YELLOW',NULL,NULL,1,NULL,NULL,NULL,375000,1),(16,12,'SD05 RED',NULL,NULL,0,'',NULL,NULL,450000,1);
 /*!40000 ALTER TABLE `tblproducts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -285,7 +290,7 @@ CREATE TABLE `tblsales` (
   `edited_at` datetime DEFAULT NULL,
   `start_at` datetime DEFAULT NULL,
   `end_at` datetime DEFAULT NULL,
-  `status` bit(1) DEFAULT NULL,
+  `status` int DEFAULT NULL,
   PRIMARY KEY (`id_sale`),
   UNIQUE KEY `id_sale_UNIQUE` (`id_sale`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -297,7 +302,7 @@ CREATE TABLE `tblsales` (
 
 LOCK TABLES `tblsales` WRITE;
 /*!40000 ALTER TABLE `tblsales` DISABLE KEYS */;
-INSERT INTO `tblsales` VALUES (1,'sale 1',NULL,NULL,NULL,NULL,_binary '\0'),(2,'sale 2',NULL,NULL,NULL,NULL,_binary '\0'),(3,'sale 3',NULL,NULL,NULL,NULL,_binary '\0'),(4,'sale 4',NULL,NULL,NULL,NULL,_binary '\0'),(5,'sale 5',NULL,NULL,NULL,NULL,_binary '\0'),(6,'sale 6',NULL,NULL,NULL,NULL,_binary '\0'),(7,'sale 7',NULL,NULL,NULL,NULL,_binary '\0'),(8,'sale 8',NULL,NULL,NULL,NULL,_binary '\0'),(9,'sale 9',NULL,NULL,NULL,NULL,_binary '');
+INSERT INTO `tblsales` VALUES (1,'sale 1',NULL,NULL,NULL,NULL,0),(2,'sale 2',NULL,NULL,NULL,NULL,0),(3,'sale 3',NULL,NULL,NULL,NULL,0),(4,'sale 4',NULL,NULL,NULL,NULL,0),(5,'sale 5',NULL,NULL,NULL,NULL,0),(6,'sale 6',NULL,NULL,NULL,NULL,0),(7,'sale 7',NULL,NULL,NULL,NULL,0),(8,'sale 8',NULL,NULL,NULL,NULL,0),(9,'sale 9',NULL,NULL,NULL,NULL,1);
 /*!40000 ALTER TABLE `tblsales` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -353,7 +358,7 @@ CREATE TABLE `tblsizedetails` (
 
 LOCK TABLES `tblsizedetails` WRITE;
 /*!40000 ALTER TABLE `tblsizedetails` DISABLE KEYS */;
-INSERT INTO `tblsizedetails` VALUES (1,1,10,0),(2,2,10,0),(3,1,23,1),(3,2,105,1),(3,3,10,0),(3,4,95,1),(3,5,47,1),(3,6,65,1),(4,4,10,0),(5,5,10,0),(6,6,10,0),(7,6,0,0),(8,6,102,1),(10,1,40,1);
+INSERT INTO `tblsizedetails` VALUES (1,1,18,1),(1,2,20,1),(1,3,20,1),(2,2,20,1),(3,1,18,1),(3,2,105,1),(3,3,10,0),(3,4,95,1),(3,5,47,1),(3,6,65,1),(4,4,10,0),(5,5,10,0),(6,6,10,0),(7,6,0,0),(8,6,102,1),(10,1,40,1),(13,1,5,0),(13,2,5,0),(16,6,10,0);
 /*!40000 ALTER TABLE `tblsizedetails` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -394,4 +399,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-15 13:19:38
+-- Dump completed on 2021-03-19 18:21:54
